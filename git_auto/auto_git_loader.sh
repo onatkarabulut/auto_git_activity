f1306aa9ffb42bbc44299f118a89701eafba0657
Alperen Aga <alperencode@gmail.com>
2023-08-30 03:31:50 +0300

    Add Argument Feature Update python and bash script

--- git_auto/auto_git_loader.sh
+++ git_auto/auto_git_loader.sh
@@ -2,8 +2,10 @@
 
 read -p "Devam etmek istiyor musunuz? (E/H): " answer
 if [[ "$answer" == "E" || "$answer" == "e" || "$answer" == "Y" || "$answer" == "y" ]]; then
+    read -p "GitHub kullanıcı adınızı giriniz: " github
+    read -p "Repository ismini giriniz: " repository
     echo "Devam ediliyor..."
-    python3 /home/onat/Masaüstü/dev/auto-gitPy/git-loader.py
+    python3 ./git-loader.py $github $repository
 
 else
     echo "İşlem iptal edildi."
--- git_auto/git_loader.py
+++ git_auto/git_loader.py
@@ -1,6 +1,11 @@
 import random
 import string
 import os
+import sys
+import platform
+
+URL = f"https://github.com/{sys.argv[1]}/{sys.argv[2]}.git"
+FILE_NAME = "random.txt"
 
 
 def generate_random_python_code(length):
@@ -8,47 +13,53 @@ def generate_random_python_code(length):
     return ''.join(random.choice(characters) for _ in range(length))
 
 
-def create_random_python_code_file(filename):
-    num_lines = random.randint(500, 1000) 
-    max_line_length = 200  # İstediğiniz maksimum satır uzunluğu
-    with open(filename, "w") as f:
+def create_random_python_code_file():
+    num_lines = random.randint(200, 500)
+    max_line_length = 100  # İstediğiniz maksimum satır uzunluğu
+    with open(FILE_NAME, "w") as f:
         for _ in range(num_lines):
             line = generate_random_python_code(max_line_length)
             f.write(line + "\n")
 
 
+def delete_temp():
+    os.chdir('..')
+
+    if platform.system() == 'Windows':
+        os.system("rmdir /s /q temp_repo")
+    else:
+        os.system('rm -rf temp_repo')
+
+
 def clean_repository():
-    os.system('git clone {} temp_repo'.format(repo_name))  # Uzak repo klonla
+    os.system(f'git clone {URL} temp_repo')  # Uzak repo klonla
     os.chdir('temp_repo')  # Temp dizinine geç
     os.system('git rm -r *')  # Mevcut bütün dosyaları temizle
     os.system('git commit -m "Clean repository"')
-    os.system('git push origin main')  # Temizlenmiş repoyu geri yükle
-    os.chdir('..')  # Önceki dizine geri dön
-    os.system('rm -rf temp_repo')  # Temp dizini sil
+    os.system('git push')  # Temizlenmiş repoyu geri yükle
 
 
-def load_file(filename):
-    os.system('git clone {} temp_repo'.format(repo_name))  # Uzak repo klonla
-    os.chdir('temp_repo')  # Temp dizinine geç
-    os.system('cp ../{} .'.format(filename))  # Hedef dosyayı temp dizinine kopyala
-    os.system('git add {}'.format(filename))
-    os.system('git commit -m "Add {}"'.format(filename))
-    os.system('git push origin main')  # Dosyayı geri yükle
-    os.chdir('..')  # Önceki dizine geri dön
-    os.system('rm -rf temp_repo')  # Temp dizini sil
+def load_file():
+    create_random_python_code_file()  # Temp dizininde random dosyayı oluştur
+    os.system('git add {}'.format(FILE_NAME))
+    os.system('git commit -m "Add {}"'.format(FILE_NAME))
+    os.system('git push')  # Dosyayı geri yükle
 
 
 def main():
-    filename = "random_python_code.py"
-    create_random_python_code_file(filename)
-    print("===============================================")
+    print("\n" + "="*10 + " Repository Temizleniyor " + "="*10)
     clean_repository()  # Önce mevcut bütün dosyaları temizle
-    print("===============================================")
-    load_file(filename)  # Sonra belirtilen dosyayı yükle
-    print("===============================================")
+    print("\n" + "="*10 + " Random Dosya Ekleniyor " + "="*10)
+    load_file()  # Sonra belirtilen dosyayı yükle
+    print("\n" + "="*10 + " Temp Dizini Siliniyor " + "="*10)
+    delete_temp()  # Temp dizinini sil
+    print("\n" + "="*10 + " Temp Dizini Silindi " + "="*10)
 
 
 if __name__ == '__main__':
-    repo_name = "https://github.com/onatkarabulut/git_loader.git"  # Öncelikle GitHub'da bir private repo oluşturun ve linkini sonuna '.git' ekleyerek girin
-    for i in range(random.randint(1, 8)):
+    if len(sys.argv) != 3:
+        print("Example usage: python git-loader.py Nickname RepositoryName")
+        exit()
+
+    for i in range(random.randint(1, 1)):
         main()
